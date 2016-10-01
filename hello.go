761a44fdee378dac62f3c1e04f82dc7d9a96e629
Goce Jankovski <jk.goce@gmail.com>
2016-10-01 18:09:13 +0000

    Second commit

---
+++ hello.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+      // "net/http"
+        
+/*
+func init() {
+    http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "Hello World!")    
+}
+*/
+
+func main() {
+    fmt.Printf("Hello World\n")
+}
